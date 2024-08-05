bdd9c2e6276fa3f3fbc884fcc0da2e6ebed4d782
john_f <john_f@192.168.1.82>
2024-08-05 19:01:36 +0300

    laravel-docker

--- app/public/index.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-$fd = fopen("robo-subs.txt", 'a+') or die("не удалось создать файл");
-$str = print_r(array("mem_id"=>1,"amount"=> 5, "date"=> date('Y-m-d H:i:s')),1);
-fwrite($fd, $str);
-fclose($fd);
